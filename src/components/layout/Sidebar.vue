ec86c538baec952a33a6035ab7112cbad98930ef
Farid Mahmudov <razormad666@gmail.com>
2025-05-27 17:51:09 +0200

    update

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -37,28 +37,28 @@ const links = ref<Array<{label: string, icon: string, to: string, disabled?: Ref
     disabled: computed(() => !appModules.value[userId.value]?.some((module) => module.dic?.length)),
   },
 ])
-
 </script>
 
 <template>
   <aside :class="{ closed: !isOpen }">
     <ul>
-      <RouterLink
-        :key="link.to"
-        :style="{
-          opacity: link.disabled ? '0.5' : '1',
-          pointerEvents: link.disabled ? 'none' : 'all',
-        }"
-        activeClass="active-link"
-        v-for="link in links"
-        class="flex my-2 d-block p-2 align-items-center gap-1 p-button-rounded p-button-text w-full"
-        :to="link.to"
-      >
-        <i :class="link.icon"></i>
-        <li :class="isOpen ? 'opacity-100' : 'opacity-0'" class="w-full">
-          {{ link.label }}
-        </li>
-      </RouterLink>
+      <template  :key="link.to" v-for="link in links">
+        <RouterLink
+          v-if="!link.disabled"
+          :style="{
+            opacity: link.disabled ? '0.5' : '1',
+            pointerEvents: link.disabled ? 'none' : 'all',
+          }"
+          activeClass="active-link"
+          class="flex my-2 d-block p-2 align-items-center gap-1 p-button-rounded p-button-text w-full"
+          :to="link.to"
+        >
+          <i :class="link.icon"></i>
+          <li :class="isOpen ? 'opacity-100' : 'opacity-0'" class="w-full">
+            {{ link.label }}
+          </li>
+        </RouterLink>
+      </template>
     </ul>
   </aside>
 </template>
